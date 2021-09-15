d5c059103d87578cf8c270cd93e40796c172df69
pash <kursorrr@gmail.com>
2021-09-15 16:56:17 +0300

    chats items

---
+++ app/src/main/java/com/pashssh/messenger/SingleChatAdapter.kt
@@ -0,0 +1,26 @@
+package com.pashssh.messenger
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pashssh.messenger.databinding.FragmentSingleChatBinding
+
+class SingleChatAdapter : RecyclerView.Adapter<SingleChatAdapter.SingleChatViewHolder>() {
+
+    inner class SingleChatViewHolder(binding: FragmentSingleChatBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+            
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SingleChatViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SingleChatViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
