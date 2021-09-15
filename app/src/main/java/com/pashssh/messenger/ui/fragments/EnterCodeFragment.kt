b24e02f250555d8ec359b48d912208f5102b1252
pash <kursorrr@gmail.com>
2021-09-15 11:27:45 +0300

    fixed code send

--- app/src/main/java/com/pashssh/messenger/ui/fragments/EnterCodeFragment.kt
+++ app/src/main/java/com/pashssh/messenger/ui/fragments/EnterCodeFragment.kt
@@ -13,29 +13,64 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
 import com.pashssh.messenger.*
 import com.pashssh.messenger.databinding.FragmentEnterCodeBinding
 import com.pashssh.messenger.ui.activities.RegistrationActivity
+import java.util.concurrent.TimeUnit
 
 class EnterCodeFragment() : Fragment() {
 
+    private lateinit var mCallback: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    private lateinit var mPhoneNumber: String
+    private var storedVerificationId: String? = ""
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentEnterCodeBinding.inflate(inflater)
+        val args = EnterCodeFragmentArgs.fromBundle(requireArguments())
+
+        mCallback = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+//                AUTH.signInWithCredential(credential).addOnCompleteListener {
+                signInWithPhoneAuthCredential(credential)
+//                }
+            }
+
+            override fun onVerificationFailed(p0: FirebaseException) {
+                Toast.makeText(
+                    requireContext(),
+                    p0.message.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                resendToken = token
+                storedVerificationId = verificationId
+
+            }
+        }
+
+        val phone = args.phoneNumber
+        startPhoneNumberVerification(phone)
+
+
 
         binding.testBtnCode.setOnClickListener {
-            val code = binding.registrationCode.text.toString()
-            enterCode(code)
+//            val code = binding.registrationCode.text.toString()
+//            enterCode(code)
         }
 
         val regCodeEditText = binding.registrationCode
@@ -49,8 +84,10 @@ class EnterCodeFragment() : Fragment() {
 
             override fun afterTextChanged(s: Editable?) {
                 val code = regCodeEditText.text.toString()
+                Log.d("MYTAG", code)
                 if (code.length == 6) {
-                    enterCode(code)
+
+                    verifyPhoneNumberWithCode(storedVerificationId, code)
 
                     val view = (activity as RegistrationActivity).currentFocus
                     view?.let { v ->
@@ -65,40 +102,75 @@ class EnterCodeFragment() : Fragment() {
         return binding.root
     }
 
-    private fun enterCode(code: String){
-        val args = EnterCodeFragmentArgs.fromBundle(requireArguments())
-        val credential = PhoneAuthProvider.getCredential(args.id, code)
+    private fun startPhoneNumberVerification(phoneNumber: String) {
+        val options = PhoneAuthOptions.newBuilder(AUTH)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60, TimeUnit.SECONDS)
+            .setActivity(this.requireActivity())
+            .setCallbacks(mCallback)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private fun verifyPhoneNumberWithCode(verificationId: String?, code: String) {
+        val credential = PhoneAuthProvider.getCredential(verificationId!!, code)
+        signInWithPhoneAuthCredential(credential)
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        Log.d("MYTAG", credential.smsCode.toString())
         AUTH.signInWithCredential(credential).addOnCompleteListener { task ->
             if (task.isSuccessful) {
+                Log.d("MYTAG", "$task task")
                 val user = task.result?.user
-                Log.d("MYTAG", user.toString() + "user")
                 if (user != null) {
-                REF_DATABASE.child(PHONES_CHILD).child(user.phoneNumber!!).setValue(user.uid)
-                    .addOnFailureListener {
-                        Toast.makeText(
-                            requireContext(), it.message.toString(), Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                    .addOnSuccessListener {
-                        REF_DATABASE.child(USERS_CHILD).child(user.uid).setValue(user.toUser())
-                            .addOnFailureListener {
-                                Toast.makeText(
-                                    requireContext(), it.message.toString(), Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                            .addOnSuccessListener {
-                                this.findNavController()
-                                    .navigate(R.id.action_enterCodeFragment_to_mainActivity)
-                            }
+                    REF_DATABASE.child(PHONES_CHILD).child(user.phoneNumber!!).setValue(user.uid)
+                        .addOnFailureListener {
+                            Toast.makeText(
+                                requireContext(), it.message.toString(), Toast.LENGTH_SHORT
+                            ).show()
                         }
-                    }
+                        .addOnSuccessListener {
+                            REF_DATABASE.child(USERS_CHILD).child(user.uid).setValue(user.toUser())
+                                .addOnFailureListener {
+                                    Toast.makeText(
+                                        requireContext(), it.message.toString(), Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                                .addOnSuccessListener {
+                                    this.findNavController()
+                                        .navigate(R.id.action_enterCodeFragment_to_mainActivity)
+                                }
+                        }
+                }
             } else {
-                Log.d("MYTAG", task.exception.toString() + "exep")
+                Log.d("MYTAG", task.exception.toString() + " exep")
                 if (task.exception is FirebaseAuthInvalidCredentialsException) {
                     Toast.makeText(requireContext(), "Неверный код", Toast.LENGTH_SHORT).show()
                 }
             }
         }
+
+//        if (it.isSuccessful) {
+//            Toast.makeText(requireContext(), "Test", Toast.LENGTH_SHORT)
+//                .show()
+//            Log.d("MYTAG", it.exception.toString() + " task2")
+////                        this@EnterPhoneFragment.findNavController().navigate(R.id.action_enterCodeFragment_to_mainActivity)
+//        } else {
+//            Toast.makeText(
+//                requireContext(),
+//                it.exception?.message.toString(),
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
     }
+
+//    private fun enterCode(code: String) {
+//        val args = EnterCodeFragmentArgs.fromBundle(requireArguments())
+//        val credential = PhoneAuthProvider.getCredential(args.id, code)
+//        AUTH.signInWithCredential(credential).addOnCompleteListener { task ->
+//
+//        }
+//    }
 
 }
--- app/src/main/java/com/pashssh/messenger/ui/fragments/EnterPhoneFragment.kt
+++ app/src/main/java/com/pashssh/messenger/ui/fragments/EnterPhoneFragment.kt
@@ -31,44 +31,11 @@ class EnterPhoneFragment : Fragment() {
 
         val binding = FragmentEnterPhoneBinding.inflate(inflater)
 
-        mCallback = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-                AUTH.signInWithCredential(credential).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        Toast.makeText(requireContext(), "Добро пожаловать", Toast.LENGTH_SHORT)
-                            .show()
-                        Log.d("MYTAG", it.exception.toString() + "task")
-//                        this@EnterPhoneFragment.findNavController().navigate(R.id.action_enterCodeFragment_to_mainActivity)
-                    } else {
-                        Toast.makeText(
-                            requireContext(),
-                            it.exception?.message.toString(),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-            }
-            override fun onVerificationFailed(p0: FirebaseException) {
-                Toast.makeText(
-                    requireContext(),
-                    p0.message.toString(),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
-                requireView().findNavController().navigate(EnterPhoneFragmentDirections.actionEnterPhoneFragmentToEnterCodeFragment(mPhoneNumber, verificationId))
-            }
-        }
+
 
         binding.testBtn.setOnClickListener {
-            mPhoneNumber = binding.registrationNumberPhone.text.toString()
-            val options = PhoneAuthOptions.newBuilder(AUTH)
-                .setPhoneNumber(mPhoneNumber)
-                .setTimeout(60, TimeUnit.SECONDS)
-                .setActivity(this.requireActivity())
-                .setCallbacks(mCallback)
-                .build()
-            PhoneAuthProvider.verifyPhoneNumber(options)
+            val phoneNumber = binding.registrationNumberPhone.text.toString()
+            this.findNavController().navigate(EnterPhoneFragmentDirections.actionEnterPhoneFragmentToEnterCodeFragment(phoneNumber))
         }
 
         return binding.root
